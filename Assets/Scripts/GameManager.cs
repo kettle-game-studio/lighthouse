1d27b91195fd95ed1c0b44f1681d6c345482a98a
SID37 <1999login@mail.ru>
2026-04-20 22:29:12 +0300

    Add phone sound

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState gameState;
+    public PlayerController player;
+    public Phone phone;
+    public Deadushka deadushka;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
